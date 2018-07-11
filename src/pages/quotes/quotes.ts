1d4b2834d796878e7a4cbbf8f55285c2f5f5c888
Jonathan <jontopanotti@gmail.com>
2018-07-11 15:00:49 -0300

    Initial Commit

---
+++ src/pages/quotes/quotes.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavParams, AlertController } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
+import { QuotesService } from '../../services/quotes';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-quotes',
+  templateUrl: 'quotes.html',
+})
+export class QuotesPage {
+  quoteGroup: {category: string, quotes: Quote[], icon: string};
+
+  constructor(private navParams: NavParams,
+              private alertCtrl: AlertController,
+              private quotesSerivce: QuotesService){}
+
+  ionViewDidLoad() {
+    this.quoteGroup = this.navParams.data;
+  }
+
+  // implements OnInit
+  // ngOnInit(){
+  //  this.quoteGroup = this.navParams.data;
+  //}
+
+  onAddToFavorite(selectedQuote: Quote){
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Confirm?',
+      buttons: [{
+        text: 'Yes, go ahead',
+        handler:() => {this.quotesSerivce.addQuoteToFavorites(selectedQuote);}
+      },
+      {
+        text: 'No, I changed my mind!',
+        role: 'cancle',
+        handler: () => {console.log('Cancelled');}
+      }]
+    });
+    alert.present();
+  }
+
+}
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FavoritesPage } from '../favorites/favorites';
+import { LibraryPage } from '../library/library';
+
+@IonicPage()
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html',
+})
+export class TabsPage {
+  favoritesPage = FavoritesPage;
+  libraryPage = LibraryPage
+
+
+}
